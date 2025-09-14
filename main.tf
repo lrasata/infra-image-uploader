f4b2f9b9409bbf722c50b79ded64bd24b26dd623
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-09-14 12:03:26 +0200

    feat: transform into module which can be imported in other projects

---
+++ main.tf
@@ -0,0 +1,22 @@
+module "image_uploader" {
+  source = "./modules/image_uploader"
+
+  region                                        = var.region
+  environment                                   = var.environment
+  api_image_upload_domain_name                  = var.api_image_upload_domain_name
+  backend_certificate_arn                       = var.backend_certificate_arn
+  uploads_bucket_name                           = var.uploads_bucket_name
+  enable_transfer_acceleration                  = var.enable_transfer_acceleration
+  lambda_upload_presigned_url_expiration_time_s = var.lambda_upload_presigned_url_expiration_time_s
+  use_bucketav                                  = var.use_bucketav
+  bucketav_sns_findings_topic_name              = var.bucketav_sns_findings_topic_name
+}
+
+
+
+
+
+
+
+
+
