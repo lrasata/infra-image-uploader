c4442cf8ad95ee9570122c6de495b88be869d991
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-09-25 21:06:46 +0200

    fix: image-uploader fix upload dependencies

--- modules/image_uploader/lambda_process_uploaded_file.tf
+++ modules/image_uploader/lambda_process_uploaded_file.tf
@@ -1,18 +1,12 @@
 resource "null_resource" "npm_install_process_uploaded_file_lambda" {
   provisioner "local-exec" {
-    command = <<EOT
-docker run --rm -v "$(echo ${abspath("${path.module}/lambda_process_uploaded_file}") | sed 's#\\#/#g; s#^\([A-Za-z]\):#/\\1#'):/var/task" \
-  -w /var/task \
-  public.ecr.aws/lambda/nodejs:20 \
-  /bin/bash -c "if [ -f package-lock.json ]; then npm ci; else npm install; fi && npm install sharp aws-sdk"
-EOT
+    command = "docker run --rm -v ${abspath("${path.module}/lambda_process_uploaded_file")}:/var/task -w /var/task public.ecr.aws/lambda/nodejs:20 /bin/bash -c \"if [ -f package-lock.json ]; then npm ci; else npm install; fi && npm install sharp aws-sdk\""
   }
 
   triggers = {
     always_run = timestamp()
   }
 }
-
 
 
 data "archive_file" "lambda_process_uploaded_file_zip" {
