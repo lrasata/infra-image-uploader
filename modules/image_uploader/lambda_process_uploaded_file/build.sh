caab6396515874b59f17b9f47fcdcd061980104d
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-09-25 20:46:03 +0200

    fix: image-uploader fix lambda upload process dependencies with script bash

--- modules/image_uploader/lambda_process_uploaded_file/build.sh
+++ modules/image_uploader/lambda_process_uploaded_file/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-set -euo pipefail
+set -e
 
-# Go into the Lambda code directory
 cd "$(dirname "$0")"
 
-# Run inside AWS Lambda Node.js 20 image so native deps (sharp) build correctly
 docker run --rm \
   -v "$PWD:/var/task" \
-  -w /var/task" \
+  -w /var/task \
   public.ecr.aws/lambda/nodejs:20 \
   /bin/bash -c "npm ci && npm install sharp aws-sdk"
