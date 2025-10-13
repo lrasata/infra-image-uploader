9a9ec6b24eb3bba7f7ce018949fbe51248ee468b
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-10-13 16:55:40 +0200

    feat: add outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "api_gateway_invoke_url" {
   description = "Public URL for invoking the API Gateway"
   value       = "https://${var.api_image_upload_domain_name}/upload-url"
 }
+
+output "uploads_bucket_id" {
+  description = "The S3 uploads bucket ID (name)"
+  value       = module.image_uploader.uploads_bucket_id
+}
+
+output "uploads_bucket_arn" {
+  description = "The ARN of the S3 uploads bucket"
+  value       = module.image_uploader.uploads_bucket_arn
+}
