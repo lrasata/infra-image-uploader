7d3f302c08d949a94e3666dbab4e461554497cab
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-11-29 19:29:20 +0100

    feat: set up CI/CD pipeline on PR approved, closed and manual deploy after merge on main branch

--- terraform/modules/file_uploader/submodules/dynamodb/variables.tf
+++ terraform/modules/file_uploader/submodules/dynamodb/variables.tf
@@ -1,10 +1,6 @@
 variable "environment" {
-  description = "Environment name (dev, staging, prod)"
+  description = "Environment name (dev, staging, prod, ...)"
   type        = string
-  validation {
-    condition     = contains(["dev", "staging", "prod"], var.environment)
-    error_message = "Environment must be dev, staging, or prod."
-  }
 }
 
 variable "app_id" {
