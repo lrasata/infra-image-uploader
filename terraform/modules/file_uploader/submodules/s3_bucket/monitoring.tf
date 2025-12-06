f8cc13ad60785aaf89b3d2f7e0ad9adef4f27bfb
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-12-06 11:36:54 +0100

    fix: cloudtrail_logs_policy

--- terraform/modules/file_uploader/submodules/s3_bucket/monitoring.tf
+++ terraform/modules/file_uploader/submodules/s3_bucket/monitoring.tf
@@ -105,11 +105,6 @@ resource "aws_s3_bucket_policy" "cloudtrail_logs_policy" {
         Principal = { Service = "cloudtrail.amazonaws.com" }
         Action    = "s3:PutObject"
         Resource  = "${aws_s3_bucket.cloudtrail_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
-        Condition = {
-          StringEquals = {
-            "s3:x-amz-acl" = "bucket-owner-full-control"
-          }
-        }
       },
       {
         Sid       = "AllowCloudTrailToUseKMS",
